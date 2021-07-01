c81ba7887163d922b2c45658205ab513c11d9af0
Kamalpreet777682 <kamal.matharu98@gmail.com>
2021-07-01 13:30:22 +0530

    Created GroupitemViewController for group list

---
+++ CPLProject/Controllers/GroupItemViewController.swift
@@ -0,0 +1,61 @@
+
+import UIKit
+
+class GroupItemViewController: UIViewController, UITextFieldDelegate {
+
+    fileprivate var groupAPI: GroupAPI!
+    var selectedGroupItem: Group!
+
+    @IBOutlet weak var groupNameLabel: UITextField! { didSet { groupNameLabel.delegate = self } }
+
+    fileprivate let groupId  = GroupAttributes.groupId.rawValue
+    fileprivate let groupName  = GroupAttributes.groupName.rawValue
+    fileprivate let groupDescription  = GroupAttributes.groupDescription.rawValue
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.setup()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    fileprivate func setup() {
+        self.groupAPI = GroupAPI.sharedInstance
+    }
+
+    // MARK Actions :
+    @IBAction func groupSaveButtonTapped(_ sender: UIBarButtonItem) {
+        if groupNameLabel.text! != "" {
+            let newDetails = getFieldValues()
+            groupAPI.saveGroup(newDetails)
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+
+    // MARK: Textfield delegates
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+
+    fileprivate func getFieldValues() -> Dictionary<String, NSObject> {
+
+        var fieldDetails = [String: NSObject]()
+        fieldDetails[groupName] = groupNameLabel.text as NSObject?
+        fieldDetails[groupId] = "1" as NSObject
+        fieldDetails[groupDescription] = "Description temp" as NSObject
+        return fieldDetails
+    }
+}
