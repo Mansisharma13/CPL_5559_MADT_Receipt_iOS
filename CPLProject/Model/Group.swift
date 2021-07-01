10423b338e90f725ae8f938beffd8dd02909b387
Mansisharma13 <c0773985@mylambton.ca>
2021-07-01 12:41:06 +0530

    Created Group model class

---
+++ CPLProject/Model/Group.swift
@@ -0,0 +1,27 @@
+//
+//  Event.swift
+//  CoreDataCRUD
+//
+//  Copyright © 2016 Jongens van Techniek. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+enum GroupAttributes: String {
+    case
+    groupId    = "groupId",
+    groupName      = "groupName",
+    groupDescription      = "groupDescription"
+}
+
+@objc(Group)
+
+/**
+    The Core Data Model: Event
+*/
+class Group: NSManagedObject {
+    @NSManaged var groupId: String
+    @NSManaged var groupName: String
+    @NSManaged var groupDescription: String
+}
